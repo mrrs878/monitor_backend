dfa112a47135b9c0fe604a09d326223c5598abbc
mrrs878 <mrrs878@foxmail.com>
2020-08-17 13:18:10 +0800

    修改docker-compose

--- src/module/db.ts
+++ src/module/db.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://database/dashboard'),
+    MongooseModule.forRoot('mongodb://monitor_database/dashboard'),
   ],
 })
 export default class DB {}
